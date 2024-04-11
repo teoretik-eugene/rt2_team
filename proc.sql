d5c27484a6765ef889862052a38c4f7eb26f5b02
Eugene Polyakov <74543647+teoretik-eugene@users.noreply.github.com>
2024-04-11 23:01:39 +0300

    True proc.sql

--- proc.sql
+++ proc.sql
@@ -1,30 +1,27 @@
-create or replace procedure insert_prof 
-    (prof_name in varchar2, prof_last_name in varchar2, disc_name in varchar2)
-    is
-    prof_id number;
-    disc_id number;
+CREATE OR REPLACE PROCEDURE INSERT_PROF 
+    (PROF_NAME IN VARCHAR2, PROF_LAST_NAME IN VARCHAR2, DISC_NAME IN VARCHAR2)
+    IS
+    PROF_ID NUMBER;
+    DISC_ID NUMBER;
 
-    cursor p1 is
-    select pr.id from PROFESSOR pr
-    where pr.name like prof_name and pr.last_name like prof_last_name;
+    CURSOR P1 IS
+    SELECT PR.ID FROM PROFESSOR PR
+    WHERE PR.NAME LIKE PROF_NAME AND PR.LAST_NAME LIKE PROF_LAST_NAME;
 
-    cursor d1 is
-    select disc.id from DISCIPLINES disc
-    where disc.name like disc_name;
+    CURSOR D1 IS
+    SELECT DISC.ID FROM DISCIPLINES DISC
+    WHERE DISC.NAME LIKE DISC_NAME;
 
 BEGIN
-    open p1;
-    FETCH p1 INTO prof_id;
+    OPEN P1;
+    FETCH P1 INTO PROF_ID;
     -- Надо сделать проверку, что он не пустой
-    open d1;
-    FETCH d1 INTO disc_id;
+    OPEN D1;
+    FETCH D1 INTO DISC_ID;
 
     INSERT INTO DISCIPLINES_PROFESSORS (DISCIPLINE_ID, PROFESSOR_ID)
-    values(prof_id, disc_id);
+    VALUES(DISC_ID, PROF_ID);
 
 END;
 /
-EXECUTE INSERT_PROF('Vladimir', 'Solovyev', 'ИССАПР');
-
-
-
+EXEC INSERT_PROF('Vladimir', 'Solovyev', 'ИССАПР');
